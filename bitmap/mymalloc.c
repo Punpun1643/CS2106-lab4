7825e02b953983512ba2eb24f1c2174892e37a0e
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2024-04-15 00:23:04 +0800

    Feat: demo1

--- bitmap/mymalloc.c
+++ bitmap/mymalloc.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mymalloc.h"
+#include "bitmap.h"
 
 char _heap[MEMSIZE] = {0};
+int sizeMemoryAllocated[MEMSIZE] = {0};
 
 // Do not change this. Used by the test harness.
 // You may however use this function in your code if necessary.
@@ -14,16 +16,36 @@ long get_index(void *ptr) {
 }
 
 void print_memlist() {
-    // Implement this to call print_map from bitmap.c
+  // Implement this to call print_map from bitmap.c
+  print_map(_heap, MEMSIZE/8);
 }
 
 // Allocates size bytes of memory and returns a pointer
 // to the first byte.
 void *mymalloc(size_t size) {
+  int index = search_map(_heap, MEMSIZE/8, size);
+
+  if (index == -1) { // not found
     return NULL;
+  } else {
+     allocate_map(_heap, index, size);
+     sizeMemoryAllocated[index] = size; // record the size of contiguous memory allocated
+     return _heap + index;
+  }
 }
 
 // Frees memory pointer to by ptr.
 void myfree(void *ptr) {
+  if (ptr == NULL) {
+    return; 
+  }
+
+  // check if ptr exceeds the heap size
+  if (ptr < (void*)_heap || ptr > (void*)(_heap + MEMSIZE)) {
+    return; 
+  }
+
+  long index = get_index(ptr);
+  free_map(_heap, index, sizeMemoryAllocated[index]);
 }
 
