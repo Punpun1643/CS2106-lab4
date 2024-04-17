d4c4bcf37bcbe333aa8e404ad2ea8b7ecaff3b55
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2024-04-17 13:58:28 +0800

    Feat: 4bf

---
+++ linkedlist/bf/mymalloc.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "mymalloc.h"
+#include "llist.h"
+
+char _heap[MEMSIZE] = {0};
+TNode *_memlist = NULL; // To maintain information about length
+
+void init_memory() {
+  TData *initialData = (TData *) malloc(sizeof(TData));
+  initialData->val = MEMSIZE;
+  TNode *initialNode = make_node(0, initialData);
+  insert_node(&_memlist, initialNode, ASCENDING); 
+}
+
+// Do not change this. Used by the test harness.
+// You may however use this function in your code if necessary.
+long get_index(void *ptr) {
+    if(ptr == NULL)
+        return -1;
+    else
+        return (long) ((char *) ptr - &_heap[0]);
+}
+
+void print_node(TNode *node) {
+  if (node != NULL) {
+    TData *data = node->pdata;
+    printf("Status: %s Start index: %d Length: %d\n", data->is_allocated ? "ALLOCATED" : "FREE", data->start_index, data->val);
+  }
+}
+
+void print_memlist() {
+  process_list(_memlist, print_node);   
+}
+// Allocates size bytes of memory and returns a pointer
+// to the first byte.
+void *mymalloc(size_t size) {
+  // traverse the linkedlist to and get the size
+  // if size is greater than the input size, allocate
+  if (_memlist == NULL) {
+    init_memory(); 
+  }
+
+  TNode *best_fit = NULL;
+  size_t min_size = SIZE_MAX;
+  
+  TNode *curr = _memlist;
+
+  while (curr != NULL) {
+    if (!curr->pdata->is_allocated && (size_t) curr->pdata->val >= size) { // found a large enough block
+      if (curr->pdata->val == size) { // size equal, don't need to split
+        curr->pdata->is_allocated = true;
+        return (void*) &_heap[curr->pdata->start_index]; 
+      } else if (curr->pdata->val < min_size) {
+        // update min_size
+        min_size = curr->pdata->val;
+
+        // update best_fit pointer
+        best_fit = curr;
+
+      }
+    }
+    curr = curr->next;
+  }
+
+  if (best_fit != NULL) {
+    TData *newData = (TData*) malloc(sizeof(TData));
+    newData->val = best_fit->pdata->val - size; // remaining free memory
+    newData->is_allocated = false;
+    newData->start_index = best_fit->pdata->start_index + size;
+
+    TNode *newNode = make_node(best_fit->pdata->start_index + size, newData);
+   
+    // update info of the curr node
+    best_fit->pdata->val = size; // allocate size memory to the curr free node
+    best_fit->pdata->is_allocated = true;
+    
+    // insert new node to end of llist
+    insert_node(&_memlist, newNode, ASCENDING);
+    return (void*)&_heap[best_fit->pdata->start_index]; 
+  }
+  return NULL;
+}
+/*
+ *TData *newData = (TData*) malloc(sizeof(TData));
+        newData->val = curr->pdata->val - size; // remaining free memory
+        newData->is_allocated = false;
+        newData->start_index = curr->pdata->start_index + size;
+
+        TNode *newNode = make_node(curr->pdata->start_index + size, newData);
+       
+        // update info of the curr node
+        curr->pdata->val = size; // allocate size memory to the curr free node
+        curr->pdata->is_allocated = true;
+        
+        // insert new node to end of llist
+        insert_node(&_memlist, newNode, ASCENDING);
+        return (void*)&_heap[curr->pdata->start_index]; 
+
+ * */
+// Frees memory pointer to by ptr.
+void myfree(void *ptr) {
+  if (ptr == NULL) {
+    return;
+  }
+  long index = get_index(ptr); 
+  
+  // get the node to be freed
+  TNode *curr = find_node(_memlist, index);
+  
+  if (curr == NULL || !curr->pdata->is_allocated) {
+    return; 
+  }
+  
+  // free the node
+  curr->pdata->is_allocated = false;
+  
+  // try to merge with succeeding block
+  if (curr->next != NULL && !curr->next->pdata->is_allocated) {
+    curr->pdata->val += curr->next->pdata->val; 
+    merge_node(_memlist, curr, SUCCEEDING); 
+  }
+
+  // try merge with prev block
+  if (curr->prev != NULL && !curr->prev->pdata->is_allocated) {
+    curr->prev->pdata->val += curr->pdata->val; // update size of combined blocks
+    merge_node(_memlist, curr, PRECEDING);
+  }
+}
+
